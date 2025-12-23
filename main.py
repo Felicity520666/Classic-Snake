ce73b351942deb7bb8a73642558df026f45860a6
Felicity520666 <yyan520666@gmail.com>
2025-12-23 09:47:00 -0500

    added more

--- main.py
+++ main.py
@@ -4,32 +4,77 @@ import turtle
 # Define program constants
 WIDTH = 500
 HEIGHT = 500
-DELAY = 400   #Milliseconds
+DELAY = 200   #Milliseconds
 
-off
+offsets = {
+    "up": (0, 20),
+    "down": (0, -20),
+    "left":(-20, 0),
+    "right": (20, 0)
+}
+
+def go_up():
+    global snake_direction
+    if snake_direction != "down":
+        snake_direction = "up"
+
+
+def go_right():
+    global snake_direction
+    if snake_direction != "left":
+        snake_direction = "right"
+
+
+def go_down():
+    global snake_direction
+    if snake_direction != "up":
+        snake_direction = "down"
+
+
+def go_left():
+    global snake_direction
+    if snake_direction != "right":
+        snake_direction = "left"
+
 
 
 def move_snake():
-stamper.clearstamps()
+    stamper.clearstamps()  #Remove the existing stamps made by stamper
 
-new_head = snake[-1].copy()
-new_head[0] += 20
+    new_head = snake[-1].copy()
+    new_head[0] += offsets[snake_direction][0]
+    new_head[1] += offsets[snake_direction][1]
 
-# Add new head to the snake's body
-snake_append(new_head)
+    # Add new head to the snake's body
+    snake.append(new_head)
 
-# Remove last segment of the snake
-snake.pop(0)
+    # Remove last segment of the snake
+    snake.pop(0)
+
+    # Draw snake the first time
+    for segment in snake:
+        stamper.goto(segment[0], segment[1])
+        stamper.stamp()
+
+    # Refresh screen
+    screen.update()
+
+    # Rinse and repeat
+    turtle.ontimer(move_snake, DELAY)
+
 
 # Create a window to do the drawing
 screen = turtle.Screen()
 screen.setup(WIDTH, HEIGHT) #Setting the dimensions of Turtle Graphics window
-screen.title("Snake")
+screen.title("Snake Game")
 screen.bgcolor("magenta")
 screen.tracer(0) # Turn off automatic animation
 
 # Event handlers
 screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_right, "Right")
+screen.
 
 # Create a turtle to do the bidding
 stamper = turtle. Turtle()
